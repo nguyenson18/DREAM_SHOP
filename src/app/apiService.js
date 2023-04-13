96be3833f041da8018f94a3477075433ddc25547
Truong son <nguyentruongsonpp@gmail.com>
2023-04-13 08:37:34 +0700

    update

--- src/app/apiService.js
+++ src/app/apiService.js
@@ -7,22 +7,18 @@ const apiService = axios.create({
 
 apiService.interceptors.request.use(
   (request) => {
-    console.log("Start Request", request);
     return request;
   },
   function (error) {
-    console.log("REQUEST ERROR", { error });
     return Promise.reject(error);
   }
 );
 
 apiService.interceptors.response.use(
   (response) => {
-    console.log("Response", response);
     return response.data;
   },
   function (error) {
-    console.log("RESPONSE ERROR", { error });
     const message = error.response?.data?.errors?.message || "Unknown Error";
     return Promise.reject({ message });
   }
